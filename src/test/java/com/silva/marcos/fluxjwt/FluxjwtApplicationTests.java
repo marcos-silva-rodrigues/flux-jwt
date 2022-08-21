5cbbacafb524e4bc55852ea649fc928f5fd92078
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-21 12:28:34 -0300

    feat: init study

---
+++ src/test/java/com/silva/marcos/fluxjwt/FluxjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silva.marcos.fluxjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
